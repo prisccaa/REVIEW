fe45ab4e82979a755a932e3d583c90c36ff48ac2
priisccaa <priscaw514@gmail.com>
2026-04-23 17:10:52 -0400

    Add stack initialization with vector in first.cpp

--- STACK/first.cpp
+++ STACK/first.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <vector>
 #include <stack>
 using namespace std;
 
 int main() {
+    stack<int, vector<int>> s;
+    // Create a stack with vector
 
-    return 0;
+    
+    return 0;   
 }
